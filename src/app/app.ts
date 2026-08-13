221569d445329f2ec8a4c0a7c93113801cda2d6c
marcella <marcellamatos24282gmail.com>
2026-08-13 16:18:02 -0300

    menu component e atleta component criado e editado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MenuComponent } from './component/menu-component/menu-component';
+import { AtletaComponent } from './component/atleta-component/atleta-component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, AtletaComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/component/atleta-component/atleta-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atleta-component',
+  imports: [],
+  templateUrl: './atleta-component.html',
+  styleUrl: './atleta-component.css',
+})
+export class AtletaComponent {}
